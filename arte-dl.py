0468ff0bad6a678089d4c4abe2be23e41c381102
Eskaton <me@amoser.homeunix.org>
2014-10-08 18:47:09 +0200

    Adjustments for new web page

--- arte-dl.py
+++ arte-dl.py
@@ -76,11 +76,18 @@ if __name__ == "__main__":
 
    enc = sys.stdout.encoding
    page = requests.get(url)
-   jUrls = set(re.findall('"([^"]*\.json)"', page.text))
+   jUrls = set(re.findall("'([^']*\.json)'", page.text))
+
+   if len(jUrls) == 0:
+      jUrls = set(re.findall('arte_vp_url="(http://.*/player/.*)"', page.text))
+
    videos = [json.loads(requests.get(u).text) for u in jUrls]
    index = 1
 
-   if len(videos) > 1:
+   if len(videos) == 0:
+      print("No videos found")
+      sys.exit(0)
+   elif len(videos) > 1:
       for video in videos:
          player = video['videoJsonPlayer']
          print("[{}] {} ({}s)".format(index, player['VTI'].encode(enc), 
@@ -102,14 +109,20 @@ if __name__ == "__main__":
    streamsList = []
 
    for key in streams.keys():
-      streamsList.append(streams[key])
+      stream = streams[key]
+      if not 'videoFormat' in stream or stream['videoFormat'] == 'RMP4':
+         streamsList.append(streams[key])
 
    streamsList = sorted(streamsList, cmp=quality)
 
    for stream in streamsList:
-      print("[{}] {} / {} bits / {} / {}".format(index, stream['quality'], 
-         stream['bitrate'], stream['videoFormat'], 
-         stream['versionLibelle'].encode(enc)))
+      if 'videoFormat' in stream:
+         print("[{}] {} / {} bits / {} / {}".format(index, stream['quality'], 
+            stream['bitrate'], stream['videoFormat'], 
+            stream['versionLibelle'].encode(enc)))
+      else:
+         print("[{}] {} / {} bits / {}".format(index, stream['quality'], 
+            stream['bitrate'], stream['versionLibelle'].encode(enc)))
       index += 1
 
    n = choose(index-1)
@@ -120,7 +133,7 @@ if __name__ == "__main__":
       outFile = streamUrl[streamUrl.rfind('/')+1:streamUrl.rfind('?')]
 
    subprocess.call(["rtmpdump", 
-      "-r", "{}mp4:{}".format(stream['streamer'], stream['url']),
+      "-r", "{}mp4:{}".format("rtmp://artestras.fcod.llnwd.net/a3903/o35/", stream['url']),
       "-W", "http://videos.arte.tv/blob/web/i18n/view/player_18-3188338-data-4921491.swf",
       "-o", outFile])
 
